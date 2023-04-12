c3eb3eb338d619de6ee1738b09c73d44c5e2daf0
Manisha1502-M <121952207+Manisha1502-M@users.noreply.github.com>
2023-04-12 10:05:46 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,22 @@
 import streamlit as st
+from PIL import Image
+import os
 
-st.header("Diamond Data Analysis")
+from pathlib import Path
+
+
+# Description
+PAGE_TITLE = "LAPTOP PRICE RECOMMENDATION"
+NAME = "Manisha Kumari"
+
+st.title("LAPTOP PRICE RECOMMENDATION")
+st.header("Manisha Kumari")
+
+
+
+
+
+
+
+
+
